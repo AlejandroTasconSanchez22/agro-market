5756cf31d682ec2a6fd761facacea7e5873e7e1b
AlejandroTasconSanchez22 <166768885+AlejandroTasconSanchez22@users.noreply.github.com>
2024-05-21 20:49:26 -0500

    Add files via upload

---
+++ AGROOMARKETT/app/src/main/java/co/com/cesde/agroomarkett/Iniciar_Session.java
@@ -0,0 +1,14 @@
+package co.com.cesde.agroomarkett;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Iniciar_Session extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciar_session);
+    }
+}
